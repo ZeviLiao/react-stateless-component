8b34562d69c0713e7b940ee494938e4f5b96dd13
Zevi Liao <jointjm@gmail.com>
2019-04-02 22:29:11 +0800

    styled-components

--- src/components/Comp2.js
+++ src/components/Comp2.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import './fish.scss'
+import React from "react"
+// import './fish.scss'
+import styled from "styled-components"
+
+const Hi = styled.h1`
+      color: blue;
+`
 
 const Comp2 = () => {
   return (
     <div>
       <div className="fish-edit">
-        <h1>Hello</h1>
+        <Hi>Hello</Hi>
       </div>
     </div>
   )
