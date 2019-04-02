c97225ed4c025d86f7a3dad7dd1221756366f0bc
Zevi Liao <jointjm@gmail.com>
2019-04-02 22:19:17 +0800

    effect all style

---
+++ src/components/Comp3.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Comp3 = () => {
+  return (
+    <div>
+      <div className="fish-edit">
+        <h1>Hello</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Comp3
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from "react"
 import { render } from "react-dom"
 import Comp2 from "./components/Comp2"
+import Comp3 from "./components/Comp3"
 
-render(<Comp2 />, document.querySelector("#main"))
+const AllComp = () => (
+  <div>
+    <Comp2 />
+    <hr />
+    <Comp3 />
+  </div>
+)
+
+render(
+  <AllComp />,
+  document.querySelector("#main"),
+)
